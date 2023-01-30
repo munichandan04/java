312c8f564ca1e12a80c76695f1b8aa61c5066419
muni chandan <123340889+munichandan04@users.noreply.github.com>
2023-01-30 20:34:49 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the 2 numbers");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = a + b;
+        System.out.println("addition= " + c);
+
+    }
+}
