d1731e734cb7266ee80573364767777365f04721
muni chandan <123340889+munichandan04@users.noreply.github.com>
2023-01-30 20:39:44 +0530

    Add files via upload

---
+++ ifconditon.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ifconditon {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("press 1");
+        int n = scan.nextInt();
+        if (n == 1) {
+            System.out.println("you pressed 1");
+
+        }
+        System.out.println("you not pressed 1");
+
+    }
+}
